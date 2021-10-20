4244fe7d8bf2bef44759693bf264e4bdc421f1af
DESKTOP-GQLE41P\Manna <hrkim@mannaplanet.co.kr>
2021-10-20 17:15:21 +0900

    hash 라우터로 새로고침 수정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as HashRouter, Route, Switch } from 'react-router-dom';
-import Header from './components/header';
+import { HashRouter, Route } from 'react-router-dom';
+
+//import Header from './components/header';
 import PostList from './components/postList';
 import Home from './components/home';
 import Footer from './components/footer';
+import Header from './components/header';
 
 function App() {
 	return (
 		<div className="App">
 			<HashRouter>
 				<Header />
-				<Switch>
-					<Route exact path={['/', '/Home']}>
+				{/*<Switch>
+					<Route
+						exact
+						path={['/react-depoly-test/', '/react-depoly-test/Home']}
+					>
 						<Home />
 					</Route>
-					<Route path="/PostList">
+
+					<Route path="/react-depoly-test/PostList">
 						<PostList />
 					</Route>
-				</Switch>
+				</Switch>*/}
+				<Route path={['/', '/Home']} exact={true} component={Home} />
+				<Route path="/PostList" component={PostList} />
 			</HashRouter>
 
 			<Footer />
